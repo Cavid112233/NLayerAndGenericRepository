074bc0d4fa2745257492ac9f5bef344ff43c091a
Cavid112233 <suleymanli535@gmail.com>
2024-05-09 04:35:53 +0400

    ..

--- NLayerAndGenericRepository/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NLayerAndGenericRepository.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

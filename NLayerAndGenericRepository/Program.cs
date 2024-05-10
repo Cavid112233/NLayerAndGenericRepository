fc039bf71d21b6c3b214bd45cbd4fe72016f45d2
Cavid112233 <suleymanli535@gmail.com>
2024-05-10 04:33:56 +0400

    finished

--- NLayerAndGenericRepository/Program.cs
+++ NLayerAndGenericRepository/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.EntityFrameworkCore;
+using Pronia.Business.Services.Abstracts;
+using Pronia.Business.Services.Concretes;
+using Pronia.Core.RepositoryAbstracts;
 using Pronia.Data.DAL;
+using Pronia.Data.RepositoryConcretes;
 using System;
 
 namespace NLayerAndGenericRepository
@@ -11,11 +16,19 @@ namespace NLayerAndGenericRepository
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllersWithViews();
 
-
             builder.Services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+            builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<IFeatureService, FeatureService>();
+            builder.Services.AddScoped<ITagService, TagService>();
+
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddScoped<IFeatureRepository, FeatureRepository>();
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddScoped<ITagRepository, TagRepository>();
 
             var app = builder.Build();
 
